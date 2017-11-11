97cda52d054a73467b13696a1b239afec7a93613
Nihar Agrawal <ngagrawa@usc.edu>
2017-11-11 03:48:56 -0500

    Constants for API Call

---
+++ backend/src/main/java/com/cv/APICall.java
@@ -0,0 +1,21 @@
+package com.cv;
+
+/**
+ * 
+ * @author Nagrawal
+ * Constants used in the API calls to Coinigy
+ */
+public class APICall {
+  public static final String CONTENT_TYPE_HEADER = "content/type";
+  public static final String X_API_KEY_HEADER = "X-API-KEY";
+  public static final String X_API_KEY_SECRET_HEADER = "X-API-KEY";
+
+  public static final String APPLICATION_JSON = "application/json";
+  public static final String EXCHANGES_URL = "https://api.coinigy.com/api/v1/exchanges";
+  public static final String MARKETS_URL = "https://api.coinigy.com/api/v1/markets";
+  public static final String MARKET_DATA_ALL_URL = "https://api.coinigy.com/api/v1/data";
+
+  public static final String DEFAULT_EXCHANGE_CODE = "GDAX";
+
+
+}
