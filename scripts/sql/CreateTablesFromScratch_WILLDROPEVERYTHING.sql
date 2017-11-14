c46f181866bcde4d035889b1b8f47d93ce9779b3
Shan <shaupt@usc.edu>
2017-11-13 22:56:56 -0800

    Commit of scripts directory to store all scripts needed for the final project

---
+++ scripts/sql/CreateTablesFromScratch_WILLDROPEVERYTHING.sql
@@ -0,0 +1,34 @@
+USE csci201l_FinalProject;
+
+DROP TABLE User;
+CREATE TABLE User (
+	userID int(11) primary key not null auto_increment,
+    username varchar (50) not null,
+    hashed_password varchar (50) not null,
+    api_access_key varchar (50),
+    api_private_key varchar (50)
+);	
+
+INSERT INTO User (username, hashed_password) VALUES ('testUser1', 'testing');
+
+
+DROP TABLE CurrencyInfo;
+CREATE TABLE CurrencyInfo(
+	currencyID int(11) primary key not null auto_increment,
+    ticker varchar (10) not null,
+    description varchar (500)
+);
+
+INSERT INTO CurrencyInfo (ticker) VALUES ('BTC');
+
+
+DROP TABLE UserWatchList;
+CREATE TABLE UserWatchList(
+	id int(11) primary key not null auto_increment,
+    userID int(11) not null,
+    currencyID int(11) not null,
+	FOREIGN KEY fk1 (userID) REFERENCES user(userID),
+    FOREIGN KEY fk2 (currencyID) REFERENCES CurrencyInfo(currencyID)
+);
+
+INSERT INTO UserWatchList (userID, currencyID) VALUES (1, 1);
