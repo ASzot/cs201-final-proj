8286b402f34c625f4169abf76857928d12883204
jinruan <jruan6377@bths.edu>
2017-11-18 01:13:01 -0800

    adding buttons to navview

--- frontend/src/router/index.js
+++ frontend/src/router/index.js
@@ -1,6 +1,8 @@
 import Vue from 'vue'
 import Router from 'vue-router'
 import NavView from '@/components/NavView'
+import register from '@/components/register.vue'
+import login from '@/components/login.vue'
 Vue.use(Router)
 
 export default new Router({
@@ -9,6 +11,14 @@ export default new Router({
       path: '/',
       name: 'NavView',
       component: NavView
+    },
+    {
+        path: '/login',
+        component: login
+    },
+    {
+    	 	path: '/register',
+    	 	component: register
     }
   ]
 })
