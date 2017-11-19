02a8f78b574374070bc07d738ac5a2443eb6e858
Andrew Szot <me@andrewszot.com>
2017-11-18 17:47:17 -0800

    Added sidebar menu navigation

---
+++ frontend/src/components/SideBarMenu.vue
@@ -0,0 +1,37 @@
+<template>
+  <v-list dense>
+    <v-list-tile v-for="item in items" :key="item.text">
+      <v-list-tile-action>
+        <v-icon>{{ item.icon }}</v-icon>
+      </v-list-tile-action>
+      <v-list-tile-content>
+        <v-list-tile-title @click="onItemSelected">
+          {{ item.text }}
+        </v-list-tile-title>
+      </v-list-tile-content>
+    </v-list-tile>
+  </v-list>
+</template>
+
+<script>
+  export default {
+    name: 'SideBarMenu',
+    methods: {
+      onItemSelected: function (event) {
+        var innerText = event.target.innerHTML.trim();
+        if (innerText == "Currencies") {
+          this.$emit("navToCurrency");
+        }
+        else {
+          console.log("Invalid element clicked!");
+          console.log(innerText);
+        }
+      }
+    },
+    data: () => ({
+      items: [
+        { icon: 'trending_up', text: 'Currencies', onClick:'onCurrency' }
+      ]
+    })
+  }
+</script>
