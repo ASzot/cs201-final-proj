fa8c784c88ae41db7c2fe965b2c79a3bfd241d6a
jinruan <jruan6377@bths.edu>
2017-11-19 03:07:41 -0800

    setting up user search pages

---
+++ backend/src/main/java/com/cv/UserSearchResponse.java
@@ -0,0 +1,13 @@
+package com.cv;
+
+public class UserSearchResponse {
+  private Vector<String> allUsers;
+
+  public DefaultResponse(Vector<String> allUsers) {
+    this.allUsers = allUsers; 
+  }
+
+  public String getUsers() {
+    return allUsers;
+  }
+}
