cd5259fc75dfae3721baef0fbf66fc8e6d9a45ae
Nihar Agrawal <ngagrawa@usc.edu>
2017-11-25 12:39:56 -0800

    Pushing cleaned moving average files

--- backend/src/main/java/com/cv/movingAverages/MovingAverageThread.java
+++ backend/src/main/java/com/cv/movingAverages/MovingAverageThread.java
@@ -17,29 +17,15 @@ public class MovingAverageThread implements Runnable {
   private String exchange;
   private int dayInterval;
   private TimeSeries ts;
-  private Long secondsInterval;
-  private Long startInterval;
-  private Long endInterval;
-  private Long seriesEnd;
   private List<Double> dayPricesCache;
-  private String market;
-  private CryptoWatchApi cryptoWatchApi;
   private List<String> dayPeriod; //dummy list to only hold epoch value of 1 day for moving average API Call calculation
   
   public MovingAverageThread(String exchange, int dayInterval, TimeSeries ts, long startGraphTimestamp, long endGraphTimestamp, String fromCur, String toCur, List<Double> dayPricesCache) {
-//    System.out.println("dayPricesCache size: " + dayPricesCache.size());
     this.dayPricesCache = dayPricesCache;
-//    System.out.println("After copy dayPricesCache size: " + this.dayPricesCache.size());
-
     this.exchange = exchange; 
     this.dayInterval = dayInterval;
     this.ts = ts;
-    secondsInterval = dayInterval * Constants.DAY_UNIX;
-    startInterval = startGraphTimestamp;
-    endInterval = startGraphTimestamp + secondsInterval;
-    seriesEnd = endGraphTimestamp;
     dayPricesCache = new ArrayList<Double>();
-    this.market = fromCur + toCur;
     dayPeriod = new ArrayList<String>();
     dayPeriod.add(String.valueOf(Constants.DAY_UNIX));
   }
@@ -101,41 +87,8 @@ public class MovingAverageThread implements Runnable {
     return intervalSum; 
   }
   
-//  private Long nextDay(Long timestamp) {
-//    return timestamp + Constants.DAY_UNIX;
-//  }
-  
   private Double calculateAverage(Double sum, long dayInterval) {
     return (sum/dayInterval);
   }
-  
-//  //Returns the closing price for a single day : [timestamp, timestamp + 1 day]
-//  private Double getPrice(String exchange, Long timestamp) {
-//    long begin = timestamp;
-//    long end = timestamp + Constants.DAY_UNIX;    
-//    
-//    CandleStickSeries candles = getCrypowatchApi().getCandlestick(market, exchange, dayPeriod, end, begin);
-//    if (candles == null || candles.getPeriods().size() > 1) {
-//      return 0.0;
-//    }
-//    
-//    //Will return 2 values -- only want to take the first one because corresponds to start interval
-//    CandleStickPoint csp = null;
-//    if (candles.getPeriods().get(Constants.DAY_UNIX).get(0) instanceof CandleStickPoint) {
-//      csp = (CandleStickPoint)candles.getPeriods().get(Constants.DAY_UNIX).get(0);
-////      System.out.println("CSP: Timestamp: " + csp.getTimestamp() + " Price: " + csp.getClose());
-//    }
-//    else {
-//      return 0.0; //error
-//    }
-//
-//    return csp.getClose();
-//  }
-  
-  private CryptoWatchApi getCrypowatchApi() {
-    if (cryptoWatchApi == null) {
-      cryptoWatchApi = new CryptoWatchApi(new CryptoWatchRest());
-    }
-    return cryptoWatchApi;
-  }
+
 }
